447254c58eeed380623caaaeb5041ced5d7a05c7
Felix Quehl <felix.quehl@icloud.com>
2024-06-10 20:27:49 +0200

    changed main to use only port 80

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,16 @@
-use std::env;
 use std::process;
 
 mod communication;
 mod processing;
 
-const ARG_COUNT: usize = 2;
+static ENDPOINT: &str = "0.0.0.0:80";
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() != ARG_COUNT {
-        eprintln!("Usage: {} <127.0.0.1:8080>", args[0]);
-        std::process::exit(1);
-    } else {
-        let tcp_endpoint = &args[1];
-        let callback = processing::process_tcp_stream;
-        let result = communication::communicate_via_tcp(tcp_endpoint, callback);
+    let callback = processing::process_tcp_stream;
+    let result = communication::communicate_via_tcp(ENDPOINT, callback);
 
-        if let Err(e) = result {
-            eprintln!("error: {}", e);
-            process::exit(1);
-        }
+    if let Err(e) = result {
+        eprintln!("error: {}", e);
+        process::exit(1);
     }
 }
